eb9d3ad51e6a590e74b762c05d390ea81337881d
avaishy <anupamvaishy2001@gmail.com>
2025-05-27 10:05:35 +0530

    Update source.jsx

--- source.jsx
+++ source.jsx
@@ -50,3 +50,49 @@ function Sources() {
 
 export default Sources;
 "\n\n\n- Ref. [1] : Page No : [9] Visa, Inc. (V) Q2 2024 Earnings Call Corrected Transcript 23-Apr-2024 1-877-FACTSET www.callstreet.com 10 Copyright © 2001-2024 FactSet CallStreet, LLC FX for the third quarter is expected to have an approximately 1 point drag to net revenue growth and approximately 1.5 point benefit to non-GAAP operating expense growth and an approximately 0.5 point drag to non-GAAP EPS growth. In summary, we had another solid quarter in Q2 with relatively stable underlying drivers and strong financial results. We feel good about the momentum in our business as we head into the second half across Consumer Payments, New Flows and Value-Added Services. We remain thoughtful with our spending plans as we continue to balance between short and long-term considerations in the context of a changing environment. So now, Jennifer, let's do some Q&A. Jennifer Como Senior Vice President & Global Head-Investor Relations, Visa, Inc. Thanks, Chris. And with that, we're ready to take questions, Holly. QUESTION AND ANSWER SECTION Operator: <br> - Ref. [2] : Page No : [6] 6% y
+ "jest": {
+    "preset": "amex-jest-preset-react",
+    "moduleNameMapper": {
+      "^uuid$": "uuid",
+      "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+      "^react-markdown$": "<rootDir>/node_modules/react-markdown/react-markdown.js",
+      "rehype-raw$": "<rootDir>/node_modules/rehype-raw/index.js"
+    },
+    "setupFiles": [
+      "./test-setup.js"
+    ],
+    "snapshotSerializers": []
+  },  C:\Users\avaishy\research-assistant-root\node_modules\rehype-raw\index.js:5
+    export {default} from './lib/index.js'
+    ^^^^^^
+
+
+      3 | import { useSelector } from 'react-redux';
+      4 | import ReactMarkdown from 'react-markdown';
+    > 5 | import rehypeRaw from 'rehype-raw';
+        | ^
+      6 | import styles from '../../../styles/sources.scss';
+      7 | import {
+      8 |   getChatMessages,
+
+      at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1505:14)
+      at Object.require (src/components/earningsCallTranscripts/chatScreen/Sources.jsx:5:1)
+      at Object.require (src/components/earningsCallTranscripts/chatScreen/ChatComponentWrapper.jsx:5:1)
+      at Object.require (src/components/earningsCallTranscripts/newSessionScreen/SessionsNav.jsx:8:1)
+      at Object.require (src/childRoutes.jsx:5:1)
+      at Object.require (src/components/Lumos.jsx:13:1)
+      at Object.require (src/index.js:1:1)
+      at Object.require (__tests__/index.spec.js:1:1)
+
+----------|---------|----------|---------|---------|-------------------
+File      | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
+----------|---------|----------|---------|---------|-------------------
+All files |       0 |        0 |       0 |       0 |
+ csp.js   |       0 |        0 |       0 |       0 |
+ index.js |       0 |        0 |       0 |       0 |
+----------|---------|----------|---------|---------|-------------------
+Test Suites: 1 failed, 1 passed, 2 total
+Tests:       1 passed, 1 total
+Snapshots:   1 passed, 1 total
+Time:        97.289 s
+Ran all test suites.
