3e326b7311d61de320313ad9ee782e94a95f5f8f
avaishy <anupamvaishy2001@gmail.com>
2025-06-27 12:36:49 +0530

    Update source.jsx

--- source.jsx
+++ source.jsx
@@ -1,7 +1,10 @@
- const res = await fetch(`${CONFIG.API_BASE_URL}/chats/get_user_session_chats`,
-        { method: 'POST',
-          credentials: 'include',
-          body: JSON.stringify(data),
-          headers: {
-            'Content-Type': 'text/plain'
-          } });
+i dont have res.status in catch block i have this onlyPOST https://lumosusersessionmgmt-dev.aexp.com/chats/get_user_session_chats net::ERR_FAILED 402 (Payment Required)
+
+   so this perticular line not trigger so i want to check in catch is error is like this then alert if (res.status === 402) {
+        setUiDisable(true);
+        window.alert('Unauthorized User - Please raise IIQ request for PRC-AXP-EH-E3-AppUser-paas-lumos-project');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Unable to load previous sessions');
+    } 
